0a6aca7cb40af8cb2b0c58c5f19c87d766f2cb09
wingsmight <wingsmight@gmail.com>
2022-03-11 01:48:04 +0700

    scale camera according to boat velocity

--- Assets/Scripts/Handlers/Camera/CameraFollow.cs
+++ Assets/Scripts/Handlers/Camera/CameraFollow.cs
@@ -16,17 +16,24 @@ public class CameraFollow : MonoBehaviour
 
     private Vector3 velocity = Vector3.zero;
     private new Camera camera;
+    private float startSize;
+    private float startLeftLimit;
 
 
     private void Awake()
     {
         this.camera = GetComponent<Camera>();
+        startSize = camera.orthographicSize;
+        startLeftLimit = leftLimit;
     }
     private void FixedUpdate()
     {
         Vector3 point = camera.WorldToViewportPoint(target.position);
         Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
         Vector3 destination = transform.position + delta;
+
+        float cameraSizeRatioDiff = camera.orthographicSize / startSize;
+        leftLimit = startLeftLimit + (cameraSizeRatioDiff - 1.0f) * camera.orthographicSize;
 
         transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
 
--- Assets/Scripts/Handlers/Camera/CameraZoom.cs
+++ Assets/Scripts/Handlers/Camera/CameraZoom.cs
@@ -5,12 +5,39 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CameraZoom : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D target;
+    [Space]
+    [SerializeField] private float factor;
+    [SerializeField] private Span sizeLimit;
+
+
     private new Camera camera;
+    private float startSize;
+    private Vector3[] childrenLocalScales = new Vector3[0];
 
 
     private void Awake()
     {
         this.camera = GetComponent<Camera>();
+        startSize = camera.orthographicSize;
+
+        childrenLocalScales = new Vector3[transform.childCount];
+        for (int i = 0; i < childrenLocalScales.Length; i++)
+        {
+            childrenLocalScales[i] = transform.GetChild(i).localScale;
+        }
+    }
+    private void Update()
+    {
+        camera.orthographicSize = startSize + target.velocity.x * factor;
+        camera.orthographicSize = Mathf.Clamp(camera.orthographicSize, sizeLimit.start, sizeLimit.finish);
+
+        float cameraSizeRatioDiff = camera.orthographicSize / startSize;
+
+        for (int i = 0; i < childrenLocalScales.Length; i++)
+        {
+            transform.GetChild(i).localScale = childrenLocalScales[i] * cameraSizeRatioDiff;
+        }
     }
 
 
