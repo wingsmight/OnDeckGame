93c80a17b94203e9cef88d7f4d8f391bf8031763
wingsmight <wingsmight@gmail.com>
2022-03-26 22:46:21 +0700

    rework parallax system

--- Assets/Scripts/Handlers/Parallax.cs
+++ Assets/Scripts/Handlers/Parallax.cs
@@ -2,29 +2,23 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    [SerializeField] private new GameObject camera;
+    [SerializeField] private new Transform cameraTransofrm;
     [SerializeField] private float parallaxEffect;
 
 
     private float startPositionX;
-    private float length;
+    private float startCameraPositionX;
 
 
-    private void Start()
+    private void Awake()
     {
         startPositionX = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        startCameraPositionX = cameraTransofrm.position.x;
     }
     private void Update()
     {
-        float temp = camera.transform.position.x * (1.0f - parallaxEffect);
-        float dist = camera.transform.position.x * parallaxEffect;
+        float offset = (cameraTransofrm.transform.position.x - startCameraPositionX) * parallaxEffect;
 
-        transform.position = new Vector3(startPositionX + dist, transform.position.y, transform.position.z);
-
-        if (temp > startPositionX + length / 1.2f)
-            startPositionX += length * 1.71f;
-        else if (temp < startPositionX - length / 1.2f)
-            startPositionX -= length * 1.71f;
+        transform.position = new Vector3(startPositionX + offset, transform.position.y, transform.position.z);
     }
 }
